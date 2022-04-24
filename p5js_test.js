fd2dc76ee06c12d82d17d87b89a064b06f44d6cb
han8051 <104290530+han8051@users.noreply.github.com>
2022-04-24 15:02:06 +0900

    Add files via upload

--- p5js_test.js
+++ p5js_test.js
@@ -2,10 +2,16 @@ function setup() {
   createCanvas(640, 360);
   stroke(255);
   noFill();
+  background(128);
 }
 
 
 function draw() {
-  background(0);
-  ellipse(width/2, height/2, 50, 50);
+  //ellipse(width/2, height/2, 50, 50);
+}
+
+function mouseClicked() {
+  fill(255,0,0);
+  ellipse(mouseX, mouseY, 50,50);
+  noFill();
 }
